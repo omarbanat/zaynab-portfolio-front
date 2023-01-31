7cf2d06031c44f41e672c4ca70690ff5e9cb23c2
BilalSerhal <bilal111.rg@gmail.com>
2023-01-31 14:40:48 +0200

    skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,19 @@
+import './Skills.css';
+import Logo from "./SkillsLogo"
+
+
+const Skills = () => {
+    return (
+        <div className="main-container">
+            <div >
+                <div className='main-container-skill'>Skills</div>
+                <Logo />
+            </div>
+
+        </div>
+
+    )
+}
+
+
+export default Skills;
