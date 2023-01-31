629dd8bbb675ff87aad6008b06acd6e848207364
BilalSerhal <bilal111.rg@gmail.com>
2023-01-31 16:35:38 +0200

    first commit in contact me

---
+++ src/components/ContactMe.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+
+const ContactMe = () => {
+    return (
+        <div className="contactme">
+            <div className="left">
+                <div className="contact-name"></div>
+                <div className="social-icon">
+                    <div className="facebook-icon"></div>
+                    <div lassName="LinkedIn-icon"></div>
+                    <div lassName="twitter-icon"></div>
+                    <div lassName="instagram-icon"></div>
+                </div>
+                <div className="personal-contact">
+                    <div className="personal-contact"></div>
+                    <div className="phone-number"> </div>
+                </div>
+                <div className="contact-email">
+                    <div className="hello-message"></div>
+                    <div className="user-email"> </div>
+                </div>
+            </div>
+            <div className="right">
+                <div className="contact-title"></div>
+                <div className="questions-part">
+                    <div className="name-question"></div>
+                    <div className="email-question"></div>
+                    <div className="message"></div>
+                    <div className="sendbtn"> <button>  </button></div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
