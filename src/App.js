9aa4bfe2be1f5960de74157454c7567fcc77b139
Maya <maya.atiah.99@outlook.com>
2023-01-31 22:29:44 +0200

    responsive nav-bar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {Route,Routes} from 'react-router-dom';
 import Home from './components/Home';
+import Services from './components/Services';
+import Skills from './components/Skills'
+import React from 'react';
 
 
 function App() {
  return(
   <div className='App'>
+   
+  
     <Navbar/>
    
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
     </Routes>
+    
+    <Services/>
+    <Skills/>
+    
     
   </div>
  )
