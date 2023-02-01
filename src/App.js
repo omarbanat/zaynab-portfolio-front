08410b84fd0e5d7a81e6a63e4f0f0241829cca73
BilalSerhal <bilal111.rg@gmail.com>
2023-02-01 09:34:43 +0200

    contct me first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import React from 'react';
 import Services from './components/Services';
+import Skills from './components/Skills';
+import ContactMe from './components/ContactMe';
 
 
 
 function App() {
   return( 
-
+    <>
     <div>
     <Services/>
     </div>
+    <div>
+    <Skills/>
+    </div>
+    <div>
+    <ContactMe/>
+    </div>
+    </>
   )
 }
 
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,45 +1,56 @@
 import React from "react";
+import './ContactMe.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faInstagram,
+    faLinkedinIn,
+    faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 
 
 const ContactMe = () => {
     return (
-        <div className="contactme">
+        <div className="contact-me">
             <div className="left">
-                <div className="contact-name"></div>
+                <div className="contact-name">Zaynab Abdl<br /> Nabi</div>
                 <div className="social-icon">
-                    <div className="facebook-icon"></div>
-                    <div lassName="LinkedIn-icon"></div>
-                    <div lassName="twitter-icon"></div>
-                    <div lassName="instagram-icon"></div>
+                    <div className="facebook-icon">
+                        <FontAwesomeIcon icon={faFacebook} size="3x" color='grey' />
+                    </div>
+                    <div lassName="LinkedIn-icon">
+                        <FontAwesomeIcon icon={faLinkedinIn} size="3x" color='grey' />
+                    </div>
+                    <div lassName="twitter-icon">
+                        <FontAwesomeIcon icon={faTwitter} size="3x" color='grey' />
+                    </div>
+                    <div lassName="instagram-icon">
+                        <FontAwesomeIcon icon={faInstagram} size="3x" color='grey' />
+                    </div>
                 </div>
-                <div className="personal-contact">
-                    <div className="personal-contact"></div>
-                    <div className="phone-number"> </div>
-                </div>
-                <div className="contact-email">
-                    <div className="hello-message"></div>
-                    <div className="user-email"> </div>
+                <div className="left-buttom">
+                    <div className="personal-contact">
+                        <div className="personal-contact">My personal contact </div>
+                        <div className="phone-number">+961 81 286</div>
+                    </div>
+                    <div className="contact-email">
+                        <div className="hello-message">Say hello, on email</div>
+                        <div className="user-email">zaynab.abdelnabi@gmail.com</div>
+                    </div>
                 </div>
             </div>
             <div className="right">
-                <div className="contact-title"></div>
+                <div className="contact-title">Contact<br />Me.</div>
                 <div className="questions-part">
-                    <div className="name-question"></div>
-                    <div className="email-question"></div>
-                    <div className="message"></div>
-                    <div className="sendbtn"> <button>  </button></div>
+                    <div className="name-question">What is your name ?*</div>
+                    <div className="email-question">What is your email?*</div>
+                    <div className="message">Write your message here*</div>
+                    <div className="sendbtn"> <button>Send</button></div>
                 </div>
 
             </div>
-
-
-
-
-
-
-
-
         </div>
     )
 }
+export default ContactMe;
