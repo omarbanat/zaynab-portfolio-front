530cd19a64f528aab9f37aa7d515dd97a9eaaf85
Maya <maya.atiah.99@outlook.com>
2023-02-03 14:47:57 +0200

    scroll-page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,121 @@
 import React from "react";
-import "./Navbar.css"
-import {Link} from "react-router-dom";
-import SocialFollow from "./SocialFollow"
+import "./Navbar.css";
+// import {Link} from "react-router-dom";
+import SocialFollow from "./SocialFollow";
 import "./SocialFollow.css";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
-const Navbar =() =>{
+const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-    const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const closeMenu = () => setIsNavExpanded(false);
 
+  const OnClickHandler = () => {
+    scroll.scrollToTop();
+  };
 
-    return(
-
+  return (
     <nav className="navigation">
+      <p className="site-title" onClick={OnClickHandler}>
+        ZAYNAB <span>ABD AL NABI</span>
+      </p>
 
-    <p className="site-title"> ZAYNAB <span>ABD AL NABI</span></p>
-
-    <button className="hamburger"  onClick={() => {
+      <button
+        className="hamburger"
+        onClick={() => {
           setIsNavExpanded(!isNavExpanded);
-        }}><FaBars/></button>
-        
-    <div
+        }}
+      >
+        <FaBars />
+      </button>
+
+      <div
         className={
-            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-          }>
-    <ul>
-        <li>
-            <Link to="/"><strong>Home</strong> </Link>
-        </li>
-        <li div="about">
-            <Link to="/about">About</Link>
-        </li>
-        <li div="experience">
-            <Link to="/experience">Experience</Link>
-        </li>
-        <li div="projects">
-            <Link to="/projects">Projects</Link>
-        </li>
-        <li div="contacts">
-            <Link to="/contacts">Contacts</Link>
-        </li>
-        <li div="blogs">
-            <Link to="/blogs">BLogs</Link>
-        </li>
-    </ul>
-    </div>
-   <div className="socailfollow"><SocialFollow /></div> 
-  </nav>
-)
-}
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li div="about">
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-200}
+              duration={900}
+              onClick={closeMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="services"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={900}
+              onClick={closeMenu}
+            >
+            Services
+            </Link>
+          </li>
+          <li div="experience">
+            <Link
+              to="/experience"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Experience
+            </Link>
+          </li>
+          <li div="projects">
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Projects
+            </Link>
+          </li>
+          <li div="blogs">
+            <Link
+              to="blogs"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              BLogs
+            </Link>
+          </li>
+          <li div="contacts">
+            <Link
+              to="contacts"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={closeMenu}
+            >
+              Contacts
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="socailfollow">
+        <SocialFollow />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
-
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,19 +5,19 @@ import "./Services.css";
 // import Design from "../images/design.png";
 // import Maintenance from "../images/maintenance.png";
 // import Development from "../images/development.png";
-import { MdDesignServices} from "react-icons/md";
-import { FaWrench} from "react-icons/fa";
-import { AiOutlineLaptop} from "react-icons/ai";
+import { MdDesignServices } from "react-icons/md";
+import { FaWrench } from "react-icons/fa";
+import { AiOutlineLaptop } from "react-icons/ai";
 const Services = () => {
   return (
-    <div className="services-container">
+    <div className="services-container" id="services">
       <div className="myServices">My Services</div>
 
       <div className="services">
         <div className="design">
           <div className="service-icon">
             {/* <img className="designimage"src={Design} alt="design icon" /> */}
-            <MdDesignServices/>
+            <MdDesignServices />
           </div>
           <div className="service-name">DESIGN</div>
           <div className="service-description">
@@ -28,7 +28,7 @@ const Services = () => {
         <div className="development">
           <div className="service-icon">
             {/* <img className="developmentimage" src={Development} alt="development" /> */}
-            <FaWrench/>
+            <FaWrench />
           </div>
           <div className="service-name">DEVELOPMENT</div>
           <div className="service-description">
@@ -39,7 +39,7 @@ const Services = () => {
         <div className="maintenance">
           <div className="service-icon">
             {/* <img className="maintenance-img" src={Maintenance} alt="Maintenance" /> */}
-            <AiOutlineLaptop/>
+            <AiOutlineLaptop />
           </div>
           <div className="service-name">MAINTENANCE</div>
           <div className="service-description">
@@ -48,13 +48,11 @@ const Services = () => {
           </div>
         </div>
       </div>
-    
     </div>
   );
 };
 
 export default Services;
-
 
 // MdDesignServices
 
