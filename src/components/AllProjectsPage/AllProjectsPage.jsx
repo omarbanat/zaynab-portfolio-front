31f93740e8250172eab38dfede2f6a243d6c3815
nour <nour.hmadi.a@gmail.com>
2023-02-08 01:49:23 +0200

    static-design-all-projects-section

---
+++ src/components/AllProjectsPage/AllProjectsPage.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import './AllProjectsPage.css'
+import projectFive from '../Projects/images/projectFIVE.png'
+
+import projectTwo from '../Projects/images/projectTWO.png';
+import projectThree from '../Projects/images/projectTHREE.png';
+import projectOne from '../Projects/images/projectONE.png';
+import projectSix from '../Projects/images/projectSIX.png';
+import projectFour from '../Projects/images/poroforo.png';
+import Footer from '../Footer';
+function AllProjectsPage() {
+  return (
+    <div>
+        <h1 className='ProjectstitleL'>Projects</h1>
+      <div className='ProjectsMainCenter' id='allProjectsPage'>
+        
+        <div className='projectscenter'>
+            <div className='tripletprojects'>
+                <div class="mydivouterp">
+                <img className="sourap" src={projectOne} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+ 
+            </div>
+            <div className='tripletprojects'>
+            <div class="mydivouterp">
+                <img className="sourap" src={projectTwo} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+
+            </div>
+            <div className='tripletprojects'>
+            <div class="mydivouterp">
+                <img className="sourap" src={projectThree} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+            </div>
+            </div>
+
+
+        <div className='projectscenter'>
+            <div className='tripletprojects'>
+            <div class="mydivouterp">
+                <img className="sourap" src={projectFour} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+            </div>
+
+            <div className='tripletprojects'>
+            <div class="mydivouterp"> 
+                <img className="sourap" src={projectFive} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+            </div>
+
+            <div className='tripletprojects'>
+            <div class="mydivouterp">
+                <img className="sourap" src={projectSix} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+            </div>
+
+        </div>
+        <div className='projectscenter'>
+            <div className='tripletprojects'>
+                <div class="mydivouterp">
+                <img className="sourap" src={projectOne} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+ 
+            </div>
+            <div className='tripletprojects'>
+            <div class="mydivouterp">
+                <img className="sourap" src={projectTwo} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+
+            </div>
+            <div className='tripletprojects'>
+            <div class="mydivouterp">
+                <img className="sourap" src={projectThree} alt="firstimage"/>
+                <button type="button" class="mybuttonoverlapp"><span>Read More</span></button>
+                </div>
+                <p className="projectstitleheader"><b>Project Title</b></p>
+                <p className="projectsdateheader">Published Date</p>
+            </div>
+            </div>
+        
+        </div>
+       <Footer/>
+    </div>
+    
+  )
+}
+
+export default AllProjectsPage
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,8 +90,8 @@ const Navbar = () => {
               to="blogs"
               spy={true}
               smooth={true}
-              offset={50}
-              duration={500}
+              offset={-300}
+              duration={900}
               onClick={closeMenu}
             >
               BLogs
