967d60f090e303b0feda53596f8fc4e4bde4ddf1
Maya <maya.atiah.99@outlook.com>
2023-02-10 16:15:23 +0200

    fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,53 +6,64 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 
+
 const About = () => {
-  const API_URL = process.env.REACT_APP_API_URL;
-  const [aboutDescription, setaboutDescription] = useState([]);
-
-  const fetchAboutInformation = async () => {
-    await axios
-      .get(`${API_URL}/infos/getAllInformation`)
-      .then((response) => setaboutDescription(response.data.data));
-    console.log('info', aboutDescription);
-  };
-
-  useEffect(() => {
-    fetchAboutInformation();
-  }, []);
-
-  const aboutData =
-    aboutDescription &&
-    aboutDescription.find((data) => data.type === 'about')
-      ? aboutDescription.find((data) => data.type === 'about')
-      : null;
+ const API_URL = process.env.REACT_APP_API_URL;
+ const [aboutDescription, setaboutDescription] = useState();
+ // const [aboutData,setAbout]=useState([])
 
 
-  return (
-    <div>
-      <div className="aboutme" id="about">
-        <div className="aboutme-title">
-          <h1>About Me</h1>
-        </div>
-        <div>
-          <p>{aboutData?.fullDescription}</p>
-        </div>
-        <div>
-          <Link
-            to="skills"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={2000}
-          >
-            <button className="about-btn">My Skills</button>
-          </Link>
-        </div>
-      </div>
-      {/* <Routes>
-      <Route path="/about" element={<About></About>}></Route>
-    </Routes> */}
-    </div>
-  );
+ const fetchAboutInformation = async () => {
+   await axios
+     .get(`${API_URL}/infos/getAllInformation`)
+     .then((response) => setaboutDescription(response.data.data.filter(response=>response.type=='about')));
+  console.log(aboutDescription)
+ };
+
+
+ useEffect(() => {
+   fetchAboutInformation();
+   
+}, []);
+
+
+
+
+
+
+ return (
+
+
+  
+   <div>
+     {aboutDescription && aboutDescription.map((x)=>{
+     return(
+     <div className="aboutme" id="about" key={x.id}>
+       <div className="aboutme-title">
+         <h1>About Me</h1>
+       </div>
+       <div>
+         <p>{x.fullDescription}</p>
+       </div>
+       <div>
+         <Link
+           to="skills"
+           spy={true}
+           smooth={true}
+           offset={-50}
+           duration={2000}
+         >
+           <button className="about-btn">My Skills</button>
+         </Link>
+       </div>
+     </div>
+     )})}
+
+
+   </div>
+ );
 };
 export default About;
+
+
+
